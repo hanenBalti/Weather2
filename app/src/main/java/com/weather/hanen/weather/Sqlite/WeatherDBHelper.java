39c1c3e9e5b8edb7e1bc0181a25933ed78c945e5
hanenBalti <hanenbalti@hotmail.fr>
2015-12-27 12:25:55 +0100

    ADD table script

--- app/src/main/java/com/weather/hanen/weather/Sqlite/WeatherDBHelper.java
+++ app/src/main/java/com/weather/hanen/weather/Sqlite/WeatherDBHelper.java
@@ -11,6 +11,18 @@ public class WeatherDBHelper extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "weather.db";
     public static final int DB_VERSION = 1;
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String COMMA_SEP = ",";
+    private static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + " (" +
+                    WeatherContract.WeatherEntry._ID + " INTEGER PRIMARY KEY," +
+                    WeatherContract.WeatherEntry.COLUMN_NAME_DAY + TEXT_TYPE + COMMA_SEP +
+                    WeatherContract.WeatherEntry.COLUMN_NAME_DESCRIPTION + TEXT_TYPE + COMMA_SEP +
+                    WeatherContract.WeatherEntry.COLUMN_NAME_HIGH_LOW + TEXT_TYPE + COMMA_SEP +
+            " )" ;
+
+    private static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + WeatherContract.WeatherEntry.TABLE_NAME;
 
     public WeatherDBHelper(Context context){
         super(context, DB_NAME,null, DB_VERSION);
@@ -18,7 +30,7 @@ public class WeatherDBHelper extends SQLiteOpenHelper {
     
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQL_CREATE_ENTRIES);
     }
 
     @Override
